68389c410cf5399ccb45e32b29b09f95b1385765
mamhesam <mamhesam1@gmail.com>
2023-02-09 15:57:59 +0330

    checking winner or draw

--- scripts/game.js
+++ scripts/game.js
@@ -31,7 +31,52 @@ function selectGameField(event) {
   selectedField.classList.add("disabled");
 
   gameData[selectedRow][selectedCol] = activePlayer + 1;
-  console.log(gameData);
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
 
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  // checking rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+  // checking columns for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+  // Diagonal: top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+  // Diagonal: top right to bottom left
+  if (
+    gameData[0][2] > 0 &&
+    gameData[0][2] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][0]
+  ) {
+    return gameData[0][2];
+  }
+  if (currentRound === 9) {
+    return -1;
+  }
+  return 0;
 }
